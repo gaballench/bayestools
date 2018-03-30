6363b486efb130305f91f8837f249de041db445d
gaballench <gaballench@gmail.com>
2018-03-30 20:44:44 -0300

    Pre-release, adding Flynn's geochronological data, preparing findParams for release, and removing LNparams as findParams is a generalization of the same approach

--- R/findParams.R
+++ R/findParams.R
@@ -1,80 +1,55 @@
-#' Function for estimation of lognormal parameters through quadratic optimization
+#' Function for estimation of probability density function parameters through quadratic optimization
 #' 
-#' @param p1,p2,p3 Quantile probabilities, 0.05, 0.50 and 0.95 by default
+#' @param p A numeric vector of quantile probabilities, 0.05, 0.50 and 0.95 by default
 #'
-#' @param q1,q2,q3 Observed quantiles, might come from a HPD from a previous study (along with a median), or from biostratigraphic constrains.
+#' @param q A numeric vector of observed quantiles, might come from a HPD from a previous study (along with a median), or from other sources of prior information. See \link{Notes}.
 #'
-#' @param params Internal params to be optimized by the optim funciton on the internal .quadraticFun function
+#' @param pdfunction A character vector (of length one) with the name of the PDF function of interest. Technically this argument supports any PDF function of the form pDIST (e.g., pnorm, ppois, pexp).
+#' 
+#' @param params A character vector with the name of the parameter(s) to optimize in the probability density function. These should match the parameter names of the respective PDF function, e.g., \code{"lambda"} in the function \code{ppois}
 #'
-#' @param output One of two possible values (unconstrained internally): "complete" and "parameters". For the latter the complete output of the optim function is returned with information on convergence and squared errors (that might be useless for simple cases) or just the parameters
+#' @param output One of two possible values: \code{"complete"} and \code{"parameters"}. For the latter the complete output of the \code{optim} function is returned with information on convergence and squared errors (that might be useless for simple cases) or just the parameters.
 #'
-#' @return Either a list with the complete output of convergence, squared errors and parameter values, or just a vector of parameter values. Depends on the value of \code{output}
+#' @return Either a list with the complete output of convergence, squared errors and parameter values, or just a vector of parameter values. Depends on the value of \code{output}.
+#' Warnings may be triggered by the function \code{optim} since the optimization is a heuristic process, whenever a given iteration results in an invalid value for a given combination of parameters, the \code{optim} function tries another combination of values but inform the user about the problem through a warning. In general these can be safely disregarded.
 #'
+#' @author Main code by Gustavo A. Ballen with important contributions in expression call structure and vectorized design by Klaus Schliep (\email{Klaus.Schliep@umb.edu}).
+#' 
 #' @examples
-#' # Find the best parameters for a lognormal density that fit the observed quantiles q1 = 1, q2 = 5, and q3 = 10, providing full output for the calculations in the form of a list
-#' LNparams(q1 = 1, q2 = 5, q3 = 10)
-#' # As above but return only the parameter values instead
-#' LNparams(q1 = 1, q2 = 5, q3 = 10, output = "parameters")
+#' # Find the best parameters for a standard normal density that fit the observed quantiles -1.644854, 0, and 1.644854, providing full output for the calculations in the form of a list
+#' findParams(q = c(-1.644854, 0, 1.644854), p = c(0.05,  0.50, 0.95), output = "complete", pdfunction = "pnorm", params = c("mean", "sd"))
 #'
+#' # Given that we have prior on the age of a fossil to be 1 - 10 Ma and that we want to model it with a lognormal distribution, fin the parameters of the PDF that best reflect the uncertainty in question (i.e., the parameters  for which the observed quantiles are 1, 5.5, and 10, assuming that we want the midpoint to reflect the mean of the PDF.
+#' findParams(q = c(1, 5.5, 10), p = c(0.05,  0.50, 0.95), output = "complete", pdfunction = "plnorm", params = c("meanlog", "sdlog"))
+#' 
 #' @note Notes on distributions
-#' pbeta(q, shape1, shape2, ncp = 0, lower.tail = TRUE, log.p = FALSE)
-#' pbinom(q, size, prob, lower.tail = TRUE, log.p = FALSE)
-#' pcauchy(q, location = 0, scale = 1, lower.tail = TRUE, log.p = FALSE)
-#' pchisq(q, df, ncp = 0, lower.tail = TRUE, log.p = FALSE)
-#' pexp(q, rate = 1, lower.tail = TRUE, log.p = FALSE)
-#' pf(q, df1, df2, ncp, lower.tail = TRUE, log.p = FALSE)
-#' pgamma(q, shape, rate = 1, scale = 1/rate, lower.tail = TRUE, log.p = FALSE)
-#' pgeom(q, prob, lower.tail = TRUE, log.p = FALSE)
-#' phyper(q, m, n, k, lower.tail = TRUE, log.p = FALSE)
-#' plnorm(q, meanlog = 0, sdlog = 1, lower.tail = TRUE, log.p = FALSE)
-#' pnbinom(q, size, prob, mu, lower.tail = TRUE, log.p = FALSE)
-#' pnorm(q, mean = 0, sd = 1, lower.tail = TRUE, log.p = FALSE)
-#' ppois(q, lambda, lower.tail = TRUE, log.p = FALSE)
-#' pt(q, df, ncp, lower.tail = TRUE, log.p = FALSE)
-#' punif(q, min = 0, max = 1, lower.tail = TRUE, log.p = FALSE)
-#' pweibull(q, shape, scale = 1, lower.tail = TRUE, log.p = FALSE)
-#' #for the dirichlet distribution there is the Compositional::diri.est
-LNparams <- function(p1 = 0.05, p2 = 0.50, p3 = 0.95, q1, q2, q3, params, output = "complete") {
-    .quadraticFun <- function(pars){
-        (plnorm(q1, pars[1], pars[2]) - p1)^2 + (plnorm(q2, pars[1], pars[2]) - p2)^2 + (plnorm(q3, pars[1], pars[2]) - p3)^2
-    }
-    res <- optim(c(1, 1), .quadraticFun)
-    # if output is set to "complete", return the whole res object, otherwise, return just the parameters 
-    if (output == "parameters") {
-        return(res$par)
-    }
-    return(res)
-}
 
-# prototype for using ellipsis args, it worked!
-encap <- function(FUN, ...) {
-    sexpr <- substitute(FUN)
-    dots <- substitute(...())
-    expr <- as.call(c(as.list(sexpr), dots))
-    print(expr)
-    print(length(dots))
-    cat(eval(expr), "\n")
-    cat(eval(substitute(expr - 1)), "\n")
-}
-
-# params MUST be a list of CHARACTER mode in the same sense as in aggregate()
-findParams <- function(p1 = 0.05, p2 = 0.50, p3 = 0.95, q1, q2, q3, output = "complete", densit, params) {
-    densit <- substitute(densit)
-    params <- sapply(params, as.name)
-    densiCall1 <- as.call(c(as.list(densit), as.list(q1), params))
-    densiCall2 <- as.call(c(as.list(densit), as.list(q2), params))
-    densiCall3 <- as.call(c(as.list(densit), as.list(q3), params))
-    quadratEq <- substitute((densiCall1 - p1)^2 +
-                            (densiCall2 - p2)^2 +
-                            (densiCall3 - p3)^2)
-    quadraticFun <- function(params) {
-        eval(quadratEq)
+findParams <- function(q, p, output = "complete", pdfunction, params) {
+    # define the initial values for optimization
+    initVals = rep(mean(q), times = length(params))
+    # construct the call as a list
+    l <- length(params)
+    cl <- vector("list", 2  + length(params))
+    cl[[1]] <- as.name(pdfunction)
+    cl[[2]] <- q
+    names(cl) <- c(NA, "q", params)
+    mode(cl) <- "call"
+    # the quadratic function will minimize the error around estimates
+    quadraticFun <- function(x) {
+        cl[3:(l+2)] <- x
+        res <- eval(cl)
+        sum((res - p)^2)
     }    
-    initVals <- rep(1, times = length(params))
-    res <- optim(initVals, quadraticFun) # ERROR SOMEWHERE AROUD HERE...
-    # if output is set to "complete", return the whole res object, otherwise, return just the parameters 
+    # pick the method for optimization based on the number of parameters
+    if (l == 1) {
+        res <- optim(initVals, quadraticFun, method = "BFGS")
+    } else {
+        res <- optim(initVals, quadraticFun)
+    }
+    # return depending on the desired output    
     if (output == "parameters") {
         return(res$par)
+    } else {
+        return(res)
     }
-    return(res)
 }
--- README.md
+++ README.md
@@ -24,3 +24,33 @@ Problems?
 ---------
 
 If you find a bug or unexpected behavior, please [file an issue](https://github.com/gaballench/bayestools/issues).
+
+Implementation and miscelaneous notes
+-------------------------------------
+
+### Why to use the median of quantiles as starting values?
+
+on the initial values... the following code will generate a lot of parameter estimates from several trials using different initial values. With a bit of large number theory, a decent estimate can be found. Here, q and p are quantile and probabilities under a given distribution, a different anonymous function where sapply varies the values of the initial values allows to get the $par element of the optim call, and the the density plot shows that overall a median estimate approaches at the correct value. The values of q come from qDIST given the probabilities of interest (generally 0.05, 0.5, and 0.95). For instance: qbeta(p = c(0.05, 0.5, 0.95), shape1 = 10, shape2 = 1) for the example below:
+
+``` r
+# X = seq... is the set of values to try, from min(q) to max(q). 
+parameters <- sapply(X = seq(0.7411344, 0.9948838, length.out = 1000),
+                     FUN = function(x) {
+                         findParams(q = c(0.7411344, 0.9330330, 0.9948838),
+                                    p = c(0.05, 0.5, 0.95),
+                                    densit = "pbeta",
+                                    params = c("shape1", "shape2"),
+                                    initVals = c(x, x))$par
+                     },
+                     simplify = TRUE)
+
+plot(density(t(parameters)[, 1]))
+```
+
+![](man/figures/README-unnamed-chunk-4-1.png)
+
+``` r
+plot(density(t(parameters)[, 2]))
+```
+
+![](man/figures/README-unnamed-chunk-4-2.png)
